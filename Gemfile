f8f615303f89c9d2975ce1f8b75bc4f5802f4dc3
Ali Aas <ali@thoughtbot.com>
2017-03-03 14:20:02 -0500

    Setup middleman blog

---
+++ Gemfile
@@ -0,0 +1,11 @@
+# If you do not have OpenSSL installed, change
+# the following line to use 'http://'
+source 'https://rubygems.org'
+
+gem "bourbon", "~> 4.2"
+gem "middleman", "~> 4.1"
+gem "middleman-blog"
+gem "middleman-autoprefixer", "~> 2.6"
+gem "middleman-livereload", "~> 3.4"
+gem 'redcarpet', '~> 3.3', '>= 3.3.3'
+gem "builder", "~> 3.0"
---
+++ config.rb
@@ -0,0 +1,46 @@
+activate :directory_indexes
+activate :autoprefixer
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  blog.layout = "layouts/application"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+set :relative_links, true
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascripts"
+set :images_dir, "assets/images"
+set :fonts_dir, "assets/fonts"
+set :layout, "layouts/application"
+
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+page "/feed.xml", layout: false
+
+configure :development do
+  activate :livereload
+end
+
+configure :build do
+  activate :relative_assets
+end
