0eafcbeb9830c18d06198b1e48a1a6a955f8b9aa
Ali Aas <ali@thoughtbot.com>
2017-06-16 17:19:04 -0400

    WIP

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -7,3 +7,19 @@ $("[data-info-show]").click(function() {
 $("[data-info-hide]").click(function() {
   $("body").removeClass("info-show");
 });
+
+$("[data-theme]").click(function() {
+  var theme = $(this).data("theme");
+
+  if ($("body").hasClass("info-show")) {
+    changeTheme(theme);
+    $("body").addClass("info-show");
+  } else {
+    changeTheme(theme);
+  }
+});
+
+function changeTheme(theme) {
+  $("body").removeClass();
+  $("body").addClass("theme-" + theme);
+}
