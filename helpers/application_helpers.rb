19c25ba44dcecb7805d7c7c03f672381eb4342ee
Ali Aas <ali@thoughtbot.com>
2017-06-16 14:57:10 -0400

    Add about section

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -7,4 +7,12 @@ module ApplicationHelpers
     return File.read(file_path) if File.exists?(file_path)
     "(SVG not found)"
   end
+
+  def form_path
+    "https://goo.gl/forms/D72gzFDcTB1w8aql2"
+  end
+
+  def material_icon("icon")
+    content_tag :i, "icon", class: "material-icons"
+  end
 end
--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,1 +1,9 @@
 //= require_tree .
+
+$("[data-info-show]").click(function() {
+  $("body").addClass("info-show");
+});
+
+$("[data-info-hide]").click(function() {
+  $("body").removeClass("info-show");
+});
