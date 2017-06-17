052ffcadcea0edbdf5cc22ac48fcd15b3f7e51cc
Ali Aas <ali@thoughtbot.com>
2017-06-16 21:07:04 -0400

    Themeing

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -8,18 +8,10 @@ $("[data-info-hide]").click(function() {
   $("body").removeClass("info-show");
 });
 
-$("[data-theme]").click(function() {
-  var theme = $(this).data("theme");
+$("[data-change-theme]").click(function() {
+  var newTheme = $(this).data("change-theme");
+  var themeWrapper = $("[data-theme]");
 
-  if ($("body").hasClass("info-show")) {
-    changeTheme(theme);
-    $("body").addClass("info-show");
-  } else {
-    changeTheme(theme);
-  }
+  themeWrapper.removeClass();
+  themeWrapper.addClass("theme-" + newTheme);
 });
-
-function changeTheme(theme) {
-  $("body").removeClass();
-  $("body").addClass("theme-" + theme);
-}
