3c90589b0d23783198b3c1495ca2cf521bd348ac
Ali Aas <ali@thoughtbot.com>
2017-06-23 14:54:31 -0400

    Persist theme across page change

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,5 +1,7 @@
-//= require_tree .
+// On Load
+setThemeFromParams();
 
+// On Click
 $("[data-info-show]").click(function() {
   $("body").addClass("info-show");
 });
@@ -9,13 +11,34 @@ $("[data-info-hide]").click(function() {
 });
 
 $("[data-change-theme]").click(function() {
-  var newTheme = $(this).data("change-theme");
-  var themeClass = $("[data-theme-class]");
-
-  themeClass.removeClass();
-  themeClass.addClass("theme-" + newTheme);
+  var newThemeName = $(this).data("change-theme");
+  setTheme(newThemeName);
 });
 
+// On Hover
 $("[data-theme-dots]").hover(function() {
   $("[data-main-overlay]").toggleClass("preview-theme");
 });
+
+// Functions
+function setTheme(themeName) {
+  var themeClass = $("[data-theme-class]");
+  themeClass.removeClass();
+  themeClass.addClass("theme-" + themeName);
+
+  $("[data-theme-param-link]").each(function() {
+    var filterLink = $(this);
+    var filterLinkHref = filterLink.attr("href").split('?')[0];
+    filterLink.attr("href", filterLinkHref + "?theme=" + themeName);
+  });
+}
+
+function setThemeFromParams() {
+  var url_string = window.location.href;
+  var url = new URL(url_string);
+  var newThemeName = url.searchParams.get("theme");
+
+  if (newThemeName != null) {
+    setTheme(newThemeName);
+  }
+}
