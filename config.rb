e1bffdf2753802cd840d2669866230b47e5fcc10
Ali Aas <alexandra.aas@upstatement.com>
2018-05-14 09:27:17 -0400

    WIP

--- config.rb
+++ config.rb
@@ -24,7 +24,10 @@ end
 activate :contentful do |f|
   f.access_token = '5081448719531193a0142ce589b1e6a31dd0b3e5de8b758e0ac4a9aca82c566a'
   f.space = { space: 'pbuxw9ebesfu' }
-  f.content_types = { characters: 'character' }
+  f.content_types = {
+    characters: 'character',
+    tags: 'tag'
+  }
 end
 
 set :relative_links, true
@@ -45,4 +48,8 @@ end
 
 configure :build do
   activate :relative_assets
+end
+
+data.space.tags.each do |id, tag|
+  proxy "#{tag}.html", "tag.html"
 end
